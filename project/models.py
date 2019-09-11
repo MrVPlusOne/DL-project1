28f3f5f19b3e9bddb28da59da3b022d33f38390d
Jiayi Wei <MrVPlusOne@gmail.com>
2019-09-11 00:04:38 -0500

    Finish training loop

--- project/models.py
+++ project/models.py
@@ -2,6 +2,8 @@ import torch
 from torch import Tensor
 import torch.nn as nn
 from typing import *
+import numpy as np
+from PIL import Image
 
 """
 Optional: Your code here
@@ -11,69 +13,96 @@ channels = [3, 16, 8, 8]
 outChannels = 8
 kernel_size = 3
 stride = 2
+padding = 0
 
 imageSize = 256
 maxOutputBytes = 8192
 maxOutputFloats = int(maxOutputBytes / 4)
-
 
 def testOutputSize():
     enc: Encoder = Encoder()
     x = torch.randn([1, 3, imageSize, imageSize], dtype=torch.float)
     r = enc.forward(x)
     print("output shape {} has {} elements. Required limit = {}".format(r.shape, r.numel(), maxOutputFloats))
-
+    dec: Decoder = Decoder()
+    r2 = dec.forward(r)
+    print("reconstructed shape: {}".format(r2.shape))
 
 def encodeLayers() -> nn.Sequential:
     layers = [
         layer
         for i in range(len(channels) - 1)
         for layer in [
-            nn.Conv2d(channels[i], channels[i + 1], kernel_size, stride),
+            nn.Conv2d(channels[i], channels[i + 1], kernel_size, stride, padding),
             nn.ReLU(True)]]
-    layers.append(nn.Conv2d(channels[-1], outChannels, kernel_size, stride))
+    layers.append(nn.Conv2d(channels[-1], outChannels, kernel_size, stride, padding))
     return nn.Sequential(*layers)
 
 
 def decodeLayers() -> nn.Sequential:
     cns = channels.copy()
     cns.reverse()
-    layers = [nn.ConvTranspose2d(outChannels, cns[0], kernel_size, stride)]
+    layers = [nn.ConvTranspose2d(outChannels, cns[0], kernel_size, stride, padding=padding)]
+
+    def outPadding(i: int) -> int:
+        if i == len(cns) - 2:
+            return 1
+        else:
+            return 0
+
     layers.extend([
         layer
         for i in range(len(cns) - 1)
         for layer in [
             nn.ReLU(True),
-            nn.ConvTranspose2d(cns[i], cns[i + 1], kernel_size, stride),
+            nn.ConvTranspose2d(cns[i], cns[i + 1], kernel_size, stride, padding=padding, output_padding=outPadding(i)),
         ]])
     return nn.Sequential(*layers)
 
 
 class Encoder(torch.nn.Module):
     def __init__(self):
-        """
-        Your code here
-        """
         super(Encoder, self).__init__()
         self.model = encodeLayers()
 
     def forward(self, img) -> Tensor:
-        """
-        Your code here
-        """
         return self.model.forward(img)
+
+    def encodeImage(self, img, device, transform) -> Tensor:
+        t = torch.tensor(np.array(img), dtype=torch.float, device=device)
+        t = t.permute(2, 0, 1)[None, :, :, :]
+        t = transform(t)
+        return self.forward(t)
 
 
 class Decoder(torch.nn.Module):
     def __init__(self):
-        """
-        Your code here
-        """
         super(Decoder, self).__init__()
         self.model = decodeLayers()
 
-    def forward(self, img):
-        """
-        Your code here
-        """
+    def forward(self, img) -> Tensor:
         return self.model.forward(img)
+
+    def decodeImage(self, t: Tensor, invTransform):
+        t = self.forward(t)
+        t = invTransform(t)
+        array = t.squeeze(0).permute(1,2,0).detach().numpy()
+        return Image.fromarray(array)
+
+class EncoderDecoder(torch.nn.Module):
+    def __init__(self):
+        super(EncoderDecoder, self).__init__()
+        self.encoder: Encoder = Encoder()
+        self.decoder: Decoder = Decoder()
+
+    def forward(self, img) -> Tensor:
+        return self.decoder(self.encoder(img))
+
+    def encodeDecodeImage(self, img, device, transform, invTransform):
+        enc: Encoder = self.encoder
+        t = enc.encodeImage(img, device, transform)
+        dec: Decoder = self.decoder
+        return dec.decodeImage(t, invTransform)
+
+
+# testOutputSize()
--- project/train.py
+++ project/train.py
@@ -2,20 +2,35 @@ import torch
 import torchvision as tv
 import torchvision.transforms as transforms
 from torch.utils.data import DataLoader
-from project.models import Encoder, Decoder
+from project.models import EncoderDecoder
+from typing import TypeVar
+import torch.nn as nn
+from itertools import chain, islice
+from torch import Tensor
+import numpy as np
 
+# %% load data
 
-# load data
+transform = transforms.Compose([
+    transforms.Resize(256),
+    transforms.ToTensor(),
+    transforms.Normalize(
+        mean=(0.4914, 0.4822, 0.4466),
+        std=(0.247, 0.243, 0.261))])
+
+invTransform = transforms.Compose([
+    transforms.Normalize(mean=[0., 0., 0.],
+                         std=[1 / 0.247, 1 / 0.243, 1 / 0.261]),
+    transforms.Normalize(mean=[-0.4914, -0.4822, -0.4466],
+                         std=[1., 1., 1.]),
+    transforms.ToPILImage()
+])
+
 
 def getDataSet(root: str, train: bool) -> DataLoader:
-    transform = transforms.Compose([
-        transforms.Resize(256),
-        transforms.ToTensor(),
-        transforms.Normalize(
-            (0.4914, 0.4822, 0.4466), (0.247, 0.243, 0.261))])
     dataSet = tv.datasets.CIFAR10(root, train=train, transform=transform,
                                   download=True)
-    loader = DataLoader(dataSet, batch_size=32,
+    loader = DataLoader(dataSet, batch_size=64,
                         shuffle=True, num_workers=4)
     return loader
 
@@ -24,10 +39,93 @@ root = "../data/CIFAR/"
 trainSet: DataLoader = getDataSet(root, True)
 testSet: DataLoader = getDataSet(root, False)
 
-# initialize model
-encoder: Encoder = Encoder()
-decoder: Decoder = Decoder()
+# %% initialize model
+T = TypeVar('T')
+device = torch.device("cpu")
 
-img0, _ = trainSet.dataset[0]
 
-encoder.forward(img0.reshape(1, 3, 256, 256)).shape
+def toDevice(m: T) -> T:
+    return m.to(device, torch.float)
+
+
+model: EncoderDecoder = EncoderDecoder()
+lossModel = nn.L1Loss()
+allParams = chain(model.parameters())
+optimizer = torch.optim.Adam(allParams, lr=1e-4, weight_decay=1e-5)
+
+# %% test on sample images
+from PIL import Image
+from pathlib import Path
+
+
+def testOnSample(fromDir: Path, toDir: Path):
+    for img_path in fromDir.glob('*.jpg'):
+        img = Image.open(img_path)
+        img = transform(img)[None, :, :, :]
+        img = model(img).squeeze(0)
+        img = invTransform(img)
+        img.save("{}/{}".format(toDir, img_path.name), "JPEG")
+
+
+# %% training loop
+import datetime
+from torch.utils.tensorboard import SummaryWriter
+
+
+def trainOnBatch(img: Tensor) -> np.array:
+    img: Tensor = toDevice(img)
+    out = model(img)
+    loss = lossModel(out, img)
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+    return loss.detach().numpy()
+
+
+def testOnBatch(img: Tensor) -> np.array:
+    img = toDevice(img)
+    with torch.no_grad():
+        out = model(img)
+        loss = lossModel(out, img)
+        return loss.detach().numpy()
+
+
+writer = SummaryWriter(log_dir="../runs")
+
+trainBatches = 3
+testBatches = min(3, len(testSet))
+
+startTime = datetime.datetime.now().ctime()
+
+
+def trainingLoop():
+    step = 0
+    for epoch in range(3):
+        print("===epoch {}===".format(epoch))
+        print('-' * trainBatches)
+        for img, _ in islice(trainSet, trainBatches):
+            lossValue = trainOnBatch(img)
+
+            writer.add_scalar('Loss/train', lossValue, step)
+            print(".", end='')
+            step += 1
+        print()
+
+        print("start testing")
+        losses = []
+        print('-' * testBatches)
+        for img, _ in islice(testSet, testBatches):
+            losses.append(testOnBatch(img))
+            print(".", end='')
+        print()
+        avgLoss = np.mean(losses)
+        writer.add_scalar('Loss/test', avgLoss, step)
+
+        saveDir = Path("../saves/{}/epoch{}".format(startTime, epoch))
+        saveDir.mkdir(parents=True)
+        testOnSample(Path("../data"), saveDir)
+
+        torch.save(model.state_dict(), '{}/state_dict.pth'.format(str(saveDir)))
+
+
+trainingLoop()
